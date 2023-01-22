799080fc4d5a90a3ba9c9debdf7f8048eacfdbb7
Dr. rer. n Detlef Groth <detlef.groth@uni-potsdam.de>
2023-01-22 09:12:39 +0100

    getting kroki example runnung

---
+++ dbpp/__main__.py
@@ -0,0 +1,24 @@
+"""
+Command-line application example for dbpp package.
+"""
+import dbpp.kroki.KrokiEncoder as KrokiEncoder
+import sys, re
+import dbpp
+def usage(argv):
+    print("Usage: python3 -m dbpp kroki URL")
+def main(argv):
+    #print("This is a test run!\n")
+    if (len(argv) == 3 and argv[1] == "kroki"):
+        #print("here")
+        if re.search("^https://.+(svg|png)/",argv[2]):
+            kroki=KrokiEncoder()
+            print(kroki.kroki2dia(argv[2]))
+    #print(argv)
+
+if __name__ == "__main__":
+    if "-h" in sys.argv or "--help" in sys.argv:
+        usage(sys.argv)
+        print("\nExample:\n\npython -m dbpp kroki https://kroki.io/plantuml/svg/eNpLzkksLlZwVKhWqAUAF10DsA==")
+        print("\nVersion:\t"+dbpp.__version__+"\nAuthor:\t"+dbpp.__author__+"\n")
+    else:
+        main(sys.argv)
