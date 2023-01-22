8dde8a8dec3a2e877483ed13a9f80a046f592818
Dr. rer. n Detlef Groth <detlef.groth@uni-potsdam.de>
2023-01-22 09:22:27 +0100

    trying to fix loading package in __init__.py

--- dbpp/__init__.py
+++ dbpp/__init__.py
@@ -13,6 +13,7 @@ Example:
     2
 """
 
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 __version__ = "0.1"
 __author__  = "Detlef Groth, University of Potsdam"
---
+++ dbpp/kroki/__main__.py
@@ -0,0 +1,5 @@
+"""
+Sample kroki runner
+"""
+
+print("I am kroki __main__.py")
